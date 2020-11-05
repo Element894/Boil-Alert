88faf8c17c6239268603fd101be77a3c0ff9675f
Element894 <73976374+Element894@users.noreply.github.com>
2020-11-04 21:28:56 -0800

    Vinh's code

--- boiling_sensor.ino
+++ boiling_sensor.ino
@@ -1,89 +1,108 @@
 #include <LiquidCrystal_I2C.h>
-#include <OneWire.h>
-#include <DallasTemperature.h>
-#include <EEPROM.h>
+#include <dht.h>
+#include "pitches.h"
 
-// Data wire is plugged into pin 2 on the Arduino
-#define ONE_WIRE_BUS 2
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+dht DHT;
+const int  potPin = A0, buttonPin = 2;
+int buzzer = 9, buttonState = 0;
+#define DHT11_PIN 7
+int target, value, seconds = 0, minutes = 10;
 
-// Setup a oneWire instance to communicate with any OneWire devices (not just Maxim/Dallas temperature ICs)
-OneWire oneWire(ONE_WIRE_BUS);
+//function prototypes
+void startLCD();
+float tare(float);
 
-// Pass our oneWire reference to Dallas Temperature. 
-DallasTemperature sensors(&oneWire);
-
-
-int BUTTON_STATE = LOW;
-static const int TARE_PIN = 3;
-// Boiling true = 1
-char Boiling = 0;
-
-void setup(void)
-{
-  // Check reference temperature
-  if (EEPROM.read(0) == 0) {
-  int Rtemp = 60;
-  } else {
-  int Rtemp = EEPROM.read(0);
-};
-  // Start serial port
+void setup() {
   Serial.begin(9600);
-  Serial.println("Beginning setup.");
-
-  // Start up the library
-  sensors.begin(); // IC Default 9 bit. If you have troubles consider upping it 12. Ups the delay giving the IC more time to process the temperature measurement
-  // Tare input
-  pinMode(TARE_PIN, INPUT_PULLUP);
+  float ftemp;
+  pinMode(buttonPin, INPUT);
+  pinMode(buzzer, OUTPUT);
+  startLCD();
+  lcd.setCursor(0, 0);
+  lcd.print("Temp = ");
+  lcd.setCursor(0, 1);
+  lcd.print("Push to set temp");
+  tare(ftemp);
+  lcd.setCursor(0,1);
+  lcd.print("Temperature set!");
+  delay(2000);
+  lcd.print("                ");
+  target = ftemp;
 }
 
-int TareAverage(void) {
-  // Number of times to average
-  int AvgNum = 5, Sum = 0;
-  float Average = 0, Temperatures [AvgNum] = {};
-  
-  for (char Tindex = 0; Tindex < AvgNum; Tindex++) {
-    sensors.requestTemperatures();
-    Temperatures[Tindex] = sensors.getTempCByIndex(0);
+void loop() {
+  delay(1500);
+  value = analogRead(potPin);
+  DHT.read11(DHT11_PIN);
+  //buttonState = digitalRead(buttonPin);
+  while(buttonState == LOW){
+    buttonState = digitalRead(buttonPin);
+    if(buttonState == HIGH)
+      break;
+  }
+  float ftemp = (DHT.temperature * 9 / 5) + 32;
+  lcd.setCursor(0, 0);
+  lcd.print("Temp = ");
+  lcd.print(ftemp, 1);
+  lcd.print("\337F");
+  lcd.setCursor(0, 1);
+  lcd.print("                 ");
+  lcd.setCursor(0, 1);
+  lcd.print("Time: ");
+  lcd.setCursor(6,1);
+  minutes -= 1;
+  lcd.print(minutes);
+  lcd.setCursor(7,1);
+  lcd.print('m');
+  while(minutes != 0){
+  seconds = 59;
+  for(int i = 0; i < 60; i++){
+    lcd.setCursor(11,1);
+    lcd.print(seconds);
+    lcd.setCursor(13,1);
+    lcd.print('s');
+    seconds--;
     delay(1000);
-  };
-  for (int a = 0; a < AvgNum; a++) {
-    Sum += Temperatures[a];
-  };
-  // Return the average as an integer so that the EEPROM can accept it
-  Serial.print("Tare temperature set to: ");
-  Serial.print(static_cast<int>(Sum/AvgNum));
-  return static_cast<int>(Sum/AvgNum);
-};
+    if(seconds == 0);
+      seconds == 59;
+    }
+    lcd.setCursor(6,1);
+    lcd.print(minutes);
+    lcd.setCursor(7,1);
+    lcd.print('m');
+    minutes--;
+  }
+  delay(1500);
+}
 
-// Passes the reference temperature to caller, created to avoid global scope variable.
-int Rtempget(void) {
-   // Check reference temperature
-  if (EEPROM.read(0) == 0) {
-  int Rtemp = 60;
-  } else {
-  int Rtemp = EEPROM.read(0);
-  };
-};
+//functions
+void startLCD() {
+  lcd.begin();
+  lcd.setCursor(0, 0);
+  lcd.print("   Starting");
+  lcd.setCursor(0, 1);
+  lcd.print("  Sensor....");
+  delay(1000);
+  lcd.clear();
+}
 
-void loop(void)
-{ 
-  // Set reference temperature within loop scope.
-  int Rtemp = Rtempget();
-  // call sensors.requestTemperatures() to issue a global temperature 
-  // request to all devices on the bus
-  sensors.requestTemperatures(); // Send the command to get temperatures
-
-  // Check if temperature is at reference point
-  if(sensors.getTempCByIndex(0)<Rtemp+2||sensors.getTempCByIndex(0)>Rtemp-2){
-    Boiling = 1;
-  };
-  // Check if button is pressed, update with requirement to be held later.
-  BUTTON_STATE = digitalRead(TARE_PIN);
-  if(BUTTON_STATE == HIGH) {
-    // Write the temperature to register 0
-    EEPROM.write(0, TareAverage());
-  };
-  Serial.print("Temperature for Device 1 is: ");
-  Serial.print(sensors.getTempCByIndex(0)); // Why "byIndex"? You can have more than one IC on the same bus. 0 refers to the first IC on the wire
-  
+float tare(float ftemp) {
+  //buttonState = digitalRead(buttonPin);
+  while(buttonState == LOW){ //TARE function
+    buttonState = digitalRead(buttonPin);
+    DHT.read11(DHT11_PIN);
+    ftemp = (DHT.temperature * 9 / 5) + 32;
+    if (buttonState == HIGH)
+      break;
+    else{
+    lcd.setCursor(7, 0);
+    lcd.print(ftemp, 1);
+    lcd.print("\337F");
+    Serial.print("Tare loop\n");
+    delay(1500);
+    }
+  }
+  Serial.print(ftemp);
+  return ftemp;
 }
